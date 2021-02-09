d9f2d5066d92e3c7281716d4534b7a03749a3018
Marius <Marius-petit@hotmail.com>
2021-02-09 16:09:46 +0100

    profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DeleteRpRequest, DeleteRpRequestModel } from 'src/apiwrapper/rp/deleteRpRequest';
 
 @Component({
   selector: 'app-profile',
@@ -10,6 +12,25 @@ export class ProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  createRp(form: NgForm) {    
+    const request: DeleteRpRequest = new DeleteRpRequest();
+    const requestModel : DeleteRpRequestModel = new DeleteRpRequestModel(7);
+    
+    requestModel.user_id = 7;
+    requestModel. = form.value.title;
+    requestModel.content = form.value.textRp;
+    requestModel.is_public = form.value.isPublic;
+    console.log(DeleteRpRequest);
+
+    request.post(requestModel)
+      .then((res: any) => {
+        console.log(res);
+      })
+      .catch(console.error);
+
+    form.reset();
   }
 
 }
