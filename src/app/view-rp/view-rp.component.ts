837fb098d351fc85ae10452950d284cdfd853b0b
stevancorre <stevancorre@gmail.com>
2021-02-09 14:07:24 +0100

    Id passing with url

--- src/app/view-rp/view-rp.component.ts
+++ src/app/view-rp/view-rp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-rp',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewRpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    
   }
 
 }
