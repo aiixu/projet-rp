466dfda2680e748e985832e6e58642694d25dbae
Hugo-29 <hugo.dumas0@gmail.com>
2021-02-09 14:38:11 +0100

    chgt view rp

--- src/app/view-rp/view-rp.component.ts
+++ src/app/view-rp/view-rp.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GetRpRequest, GetRpRequestModel, GetRpResponseModel } from 'src/apiwrapper/rp/getRpRequest';
 
 @Component({
   selector: 'app-view-rp',
@@ -7,12 +8,28 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./view-rp.component.css']
 })
 export class ViewRpComponent implements OnInit {
+  response : GetRpResponseModel;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     console.log(id);
+
+    const request: GetRpRequest = new GetRpRequest();
+// on créé un objet qui est le contenu de la requête
+const requestModel: GetRpRequestModel = new GetRpRequestModel(Number(id));
+
+// on envoie la requête
+request.get(requestModel)
+  // une fois qu'elle a retourné quelque chose,
+  .then((res) => {
+    // on la stock dans le component
+    this.response = res;
+  })
+  // en cas d'erreur, on l'affiche dans la console
+  .catch(console.error);
+
     
   }
 
